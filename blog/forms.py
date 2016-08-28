801efd26979f2a5e4327e02dec5fdf31677c42d3
CherryShen <cshen@wellesley.edu>
2016-08-28 14:16:42 -0700

    removed footer, made password input

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import Form
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
-    password = StringField("password", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
 
 
 class EditForm(Form):
