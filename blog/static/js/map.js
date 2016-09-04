5b55bf104b1f652c7b28141adc30c54a0ca9637a
CherryShen <cshen@wellesley.edu>
2016-09-03 18:57:09 -0700

    styled post blog entry button

--- blog/static/js/map.js
+++ blog/static/js/map.js
@@ -1,5 +1,5 @@
 var mapCanvas = document.getElementById("map");
 var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+    center: new google.maps.LatLng(37.8, -122.4), zoom: 10
 }
 var map = new google.maps.Map(mapCanvas, mapOptions);
